884d0e0667e59e2b1188e3a14aaf9833881b5f7d
Md. Sajedul Islam <sajedulislms@gmail.com>
2020-12-01 22:44:06 +0600

    Update

--- finalLabTask1/Bank/Controller/action_login.php
+++ finalLabTask1/Bank/Controller/action_login.php
@@ -1,23 +1,13 @@
 
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bank";
-$balance="";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+include '../model/BankModel.php';
 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-
-  $uname = $psw = "";
+  $username = $psw = "";
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $counter = 0;
       if (isset($_POST["username"]) & !empty($_POST["username"])) {
-       $uname = $_POST["username"];
+       $username = $_POST["username"];
       }
       else {
         $counter = $counter + 1;
@@ -31,31 +21,20 @@ if ($conn->connect_error) {
 
       $userFound = false;
 
-      
-      $sql = "SELECT * FROM `users` WHERE username=' ".$uname." '  and password='".$psw."'";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-
-  while($row = $result->fetch_assoc()) {
-
-$userFound=true;
-$balance=$row["bslance"];
-
-    
-  }
-} else {
-  echo "0 results";
-}
 
 
-$conn->close();
+     $model=new BankModel();
+     $userFound=$model->Login($username,$psw);
+
+
+
+     if($userFound==true){
+       $counter=0;
+     }
+     else{
+       $counter=$counter+1;
+     }
         
-          if($userFound == false) {
-            $counter = $counter + 1;
-          }
-
           if($counter == 0) {
              echo "<p>Login Successful</p>";
             echo "<script> window.location.assign('../view/home.php'); </script>";
@@ -69,14 +48,12 @@ $conn->close();
 
 ?>
 <?php 
-     if($userFound){
+     if($userFound==true){
       session_start();
-      $_SESSION["id"]=$uname;
-      $_SESSION["balance"]=$balance;
+      $_SESSION["id"]=$username;
      }
      else{
       echo "<br/> <br/>Username or Password incorrecy";
      }
     ?>
-
 
