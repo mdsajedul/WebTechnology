29df8063c7900f2b3f15c0f64b58b950e5883344
Md. Sajedul Islam <sajedulislms@gmail.com>
2020-12-01 22:41:50 +0600

    Update home.php

--- finalLabTask1/Bank/View/home.php
+++ finalLabTask1/Bank/View/home.php
@@ -9,11 +9,42 @@ session_start();
 </head>
 <body>
 
+
+<?php
+		require ('../controller/action_balance.php');
+		//require_once '../model/BalanceModel.php';
+		//$model=new BankModel();
+    ?>
+
 <div>
 	<p> Welcome  <span><?php echo $_SESSION["id"] ?> </span></p>
-	<p> Your Balance is : <span><?php echo $_SESSION["balance"] ?></span>
-</div>
+	
 
+	<table>
+        <thead>
+            <tr>
+            <th>Username</th>
+            <th>Balance</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td><?php echo $uname; ?></td>
+            <td><?php echo $balance; ?></td>
+            </tr>
+        </tbody>
+        </table>
+
+</div>
+<div>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <label for="amount">Enter amount:</label>
+    <input type="text" name="amount",value="<?php echo $amount;?>" required>
+    <br/>
+    <button type="submit" value="diposit" name="diposit" >Diposit</button>
+    <button type="submit" value="withdraw" name="withdraw" >Withdraw</button>
+    </form>
+</div>
 
 </body>
 </html>
